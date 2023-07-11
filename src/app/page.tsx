8426bfe0635346fe6ffe2d3dc4487937252df45c
Raghunath Senthilnathan <1000063700@hexaware.com>
2023-07-11 22:23:22 +0530

    nextjs changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@
 
 import { Suspense } from "react";
 import Image from "next/image";
-import Context from "./context/contextApi";
-import Header from "./components/Header";
-import Feed from "./components/Feed";
-import SearchResult from "./components/SearchResult";
-import { Loading } from "./shared/Loader";
+import Context from "../context/contextApi";
+import Header from "../components/Header";
+import Feed from "../components/Feed";
+import SearchResult from "../components/SearchResult";
+import { Loading } from "../shared/Loader";
 
 export default function Home() {
   return (
