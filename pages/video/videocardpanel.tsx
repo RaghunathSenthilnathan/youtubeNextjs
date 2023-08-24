639827e96c0067677509430cab787824da317fcf
Raghunath Senthilnathan <1000063700@hexaware.com>
2023-08-24 16:59:01 +0530

    Final push

--- pages/video/videocardpanel.tsx
+++ pages/video/videocardpanel.tsx
@@ -54,78 +54,81 @@ const VideoCardPanel = () => {
 
   return (
     <div className="flex justify-center flex-row h-[calc(100%-56px)] bg-black">
-      {loading && <Loader />}
-      <div className="w-full max-w-[1280px]  flex flex-col justify-center lg:flex-row">
-        <div className="flex flex-col lg:w-[calc(100%-350px)] xl:w-[calc100%-400px] px-4 py-3 lg:py-6 overflow-y-auto">
-          <div className="h-[200px] md:h-[400px] lg:h-[400px] xl:h-[550px] ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0">
-            <ReactPlayer
-              url={`https://www.youtube.com/watch?v=${id}`}
-              controls
-              width="100%"
-              height="100%"
-              style={{ backgroundColor: "#000000" }}
-            />
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="w-full max-w-[1280px]  flex flex-col justify-center lg:flex-row">
+          <div className="flex flex-col lg:w-[calc(100%-350px)] xl:w-[calc100%-400px] px-4 py-3 lg:py-6 overflow-y-auto">
+            <div className="h-[200px] md:h-[400px] lg:h-[400px] xl:h-[550px] ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0">
+              <ReactPlayer
+                url={`https://www.youtube.com/watch?v=${id}`}
+                controls
+                width="100%"
+                height="100%"
+                style={{ backgroundColor: "#000000" }}
+              />
+            </div>
+            <div className="text-white font-bold text-sm md:text-xl mt-4 line-clamp-2">
+              {video?.title}
+            </div>
+            <div className=" flex justify-between flex-col md:flex-row mt-4">
+              <div className="flex">
+                <div className="flex items-start">
+                  <div className=" flex h-11 w-11 rounded-full overflow-hidden">
+                    <Image
+                      className="h-full w-full object-cover"
+                      src={video?.author?.avatar[0].url}
+                      height={100}
+                      width={100}
+                      alt="Avatar Image"
+                    />
+                  </div>
+                </div>
+                <div className=" flex flex-col ml-3">
+                  <div className=" text-white text-md font-semibold flex items-center">
+                    {video?.author?.title}
+                    {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
+                      <BsFillCheckCircleFill className="text-white/[0.5] text-[12px] ml-1" />
+                    )}
+                  </div>
+                  <div className=" text-white/[0.7] text-sm">
+                    {video?.author?.stats?.subscribersText}
+                  </div>
+                </div>
+              </div>
+              <div className=" flex text-white mt-4 md:mt-0">
+                <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]">
+                  <AiOutlineLike className=" text-xl text-white mr-2" />
+                  <span>
+                    {`${abbreviateNumber(video?.stats?.likes, 2)} Likes`}
+                  </span>
+                </div>
+                <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]">
+                  <AiOutlineLike className=" text-xl text-white mr-2" />
+                  <span>
+                    {`${abbreviateNumber(
+                      video?.stats?.views as number,
+                      2
+                    )} Views`}
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="text-white font-bold text-sm md:text-xl mt-4 line-clamp-2">
-            {video?.title}
-          </div>
-          <div className=" flex justify-between flex-col md:flex-row mt-4">
-            <div className="flex">
-              <div className="flex items-start">
-                <div className=" flex h-11 w-11 rounded-full overflow-hidden">
-                  <Image
-                    className="h-full w-full object-cover"
-                    src={video?.author?.avatar[0].url}
-                    height={100}
-                    width={100}
-                    alt="Avatar Image"
+          <div className=" flex flex-col py-6 px-4 overflow-y-auto lg: w-[350px] xl:w-[400px]">
+            {relatedVideos?.contents?.map((item, index) => {
+              if (item?.type !== "video") return false;
+              else
+                return (
+                  <SuggestionVideosCard
+                    key={item?.video?.videoId as string}
+                    video={item?.video}
                   />
-                </div>
-              </div>
-              <div className=" flex flex-col ml-3">
-                <div className=" text-white text-md font-semibold flex items-center">
-                  {video?.author?.title}
-                  {video?.author?.badges[0]?.type === "VERIFIED_CHANNEL" && (
-                    <BsFillCheckCircleFill className="text-white/[0.5] text-[12px] ml-1" />
-                  )}
-                </div>
-                <div className=" text-white/[0.7] text-sm">
-                  {video?.author?.stats?.subscribersText}
-                </div>
-              </div>
-            </div>
-            <div className=" flex text-white mt-4 md:mt-0">
-              <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]">
-                <AiOutlineLike className=" text-xl text-white mr-2" />
-                <span>
-                  {`${abbreviateNumber(video?.stats?.likes, 2)} Likes`}
-                </span>
-              </div>
-              <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]">
-                <AiOutlineLike className=" text-xl text-white mr-2" />
-                <span>
-                  {`${abbreviateNumber(
-                    video?.stats?.views as number,
-                    2
-                  )} Views`}
-                </span>
-              </div>
-            </div>
+                );
+            })}
           </div>
         </div>
-        <div className=" flex flex-col py-6 px-4 overflow-y-auto lg: w-[350px] xl:w-[400px]">
-          {relatedVideos?.contents?.map((item, index) => {
-            if (item?.type !== "video") return false;
-            else
-              return (
-                <SuggestionVideosCard
-                  key={item?.video?.videoId as string}
-                  video={item?.video}
-                />
-              );
-          })}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
